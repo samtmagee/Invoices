89dd5374fdba67c84477ce86d8d6d09406ffac6f
Sam Magee <sam_t_magee@hotmail.com>
2023-12-03 20:51:02 +1300

    HTML base

---
+++ template_statements.ps1
@@ -0,0 +1,18 @@
+$body = "<!DOCTYPE html>
+    <html>
+    <head>
+    <style>
+    body {background-color: white;}
+    h1   {color: black;}
+    p    {color: black;}
+    </style>
+    </head>
+    <body>
+
+    <h1>Statement</h1>
+    <p>This is a paragraph.</p>
+    "
+$body += "<p>something</p>"
+$body += "
+    </body>
+    </html>"
